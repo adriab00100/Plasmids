50c01d6abceee9b865ac9420a4ed094ffc8964aa
Brian Adriance <adriab00100@gmail.com>
2017-02-04 16:31:32 -0500

    introducing vector class

--- src/java/com/plasmids/model/Plasmid.java
+++ src/java/com/plasmids/model/Plasmid.java
@@ -1,6 +1,16 @@
 package com.plasmids.model;
 
 class Plasmid {
+    private final String sequence;
+    public String getSequence() {
+        return sequence;
+    }
+    
+    // vectors
+    
+    public Plasmid(String sequence) {
+        this.sequence = sequence;
+    }
     
     
 }
---
+++ src/java/com/plasmids/model/Vector.java
@@ -0,0 +1,7 @@
+
+package com.plasmids.model;
+
+
+public class Vector {
+    
+}
